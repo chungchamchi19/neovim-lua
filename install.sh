1eeab5603ac68366ef0285db1146e7bef30ad3fc
chungchamchi <dinhchungkhtnk29@gmail.com>
2022-03-05 13:22:05 +0700

    feat: formater and gitsign

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+npm i -g pyright && \
+npm i -g prettier && \
+npm i -g eslint && \
+npm i -g gofmt
+
---
+++ lua/plugins/formater.lua
@@ -0,0 +1,45 @@
+local js_and_friends = {
+  -- prettier
+  function()
+    return {
+      exe = "prettier",
+      args = {
+        "--stdin-filepath",
+        vim.api.nvim_buf_get_name(0),
+        -- "--single-quote",
+        "--print-width 160"
+      },
+      stdin = true,
+    }
+  end,
+}
+require("formatter").setup {
+  logging = false,
+  filetype = {
+    javascriptreact = js_and_friends,
+    javascript = js_and_friends,
+    typescriptreact = js_and_friends,
+    typescript = js_and_friends,
+    vue = js_and_friends,
+    html = js_and_friends,
+    css = js_and_friends,
+    scss = js_and_friends,
+    go = {
+      function()
+        return { exe = "gofmt", args = {}, stdin = true }
+      end,
+    },
+    python = {
+      function()
+        return {
+          exe = "autopep8",
+          args = {
+            "--in-place --aggressive --aggressive",
+            vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+          },
+          stdin = false,
+        }
+      end,
+    },
+  },
+}
