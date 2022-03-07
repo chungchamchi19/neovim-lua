b2aa6834dc2b6d6bb2e506ce18ab01116b3bbee2
chungchamchi <dinhchungkhtnk29@gmail.com>
2022-03-07 09:27:49 +0700

    feat: auto install packer in any machine

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -1,3 +1,9 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
 require("packer").startup(function()
   use "wbthomason/packer.nvim"
   use 'folke/tokyonight.nvim'
@@ -59,4 +65,8 @@ require("packer").startup(function()
     },
     -- tag = 'release' -- To use the latest release
   }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
