ee1cc927203af2c034621849fc5fcf4bfde5f93b
chungchamchi <dinhchungkhtnk29@gmail.com>
2022-03-11 08:41:42 +0700

    fix: disable tab to jump

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -64,8 +64,6 @@ cmp.setup {
         cmp.select_next_item()
       elseif luasnip.expandable() then
         luasnip.expand()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
       elseif check_backspace() then
         fallback()
       else
@@ -78,8 +76,6 @@ cmp.setup {
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
       else
         fallback()
       end
