362014c0a37af93b0145264083cd6cc4e7c707fa
chungchamchi <dinhchungkhtnk29@gmail.com>
2022-12-19 17:51:49 +0700

    feat: null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -3,10 +3,12 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettier,
-    null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.code_actions.eslint,
-    null_ls.builtins.diagnostics.cspell, 
+    null_ls.builtins.diagnostics.eslint.with({
+			diagnostics_format = "[eslint] #{m}\n(#{c})",
+		}),
+    null_ls.builtins.diagnostics.codespell.with({ 
+      filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact", "vue" } 
+    }),
     null_ls.builtins.completion.tags,
   },
-  debounce = 150,
 })
