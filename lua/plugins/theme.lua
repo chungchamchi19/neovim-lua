f43e7bdb6b4d3de66500f9ff320be99b4e399a18
chungchamchi <dinhchungkhtnk29@gmail.com>
2023-06-06 13:29:59 +0700

    feat: theme 0.9

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,25 @@
+require("tokyonight").setup({
+  style = "storm",
+  light_style = "day", 
+  transparent = false,
+  terminal_colors = true, 
+  styles = {
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {
+      italic = true,
+    },
+    variables = {},
+    sidebars = "dark", -- style for sidebars, see below
+    floats = "dark", -- style for floating windows
+  },
+  sidebars = { "qf", "help" }, 
+  day_brightness = 0.3, 
+  hide_inactive_statusline = false, 
+  dim_inactive = false,
+  lualine_bold = false, 
+  on_colors = function(colors) end,
+  on_highlights = function(highlights, colors) end,
+})
 -- theme
-vim.g.tokyonight_style = "storm" 
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_italic_variables = false
 vim.cmd [[colorscheme tokyonight]]
